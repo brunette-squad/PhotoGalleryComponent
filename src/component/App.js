f102f33a7f6a91935ca2e0022c81877b2c915c23
Erin Gurley <erinmgurley@gmail.com>
2019-05-21 16:02:15 -0700

    rendered header

--- src/component/App.js
+++ src/component/App.js
@@ -1,20 +1,28 @@
-import Component from '../component/Component.js';
+import Component from './Component.js';
+import Header from './Header.js';
 
 class App extends Component {
 
     render() {
         const dom = this.renderDOM();
 
+        const header = new Header();
+        const headerDOM = header.render();
+
+        const main = dom.querySelector('main');
+        dom.insertBefore(headerDOM, main);
         return dom;
     }
 
     renderTemplate(){
         return /*html*/`
+        <div>
             <main>
                 <ul id="photos">
-                <li>CATS</li>
+                    <li>CATS</li>
                 </ul>
             </main>
+        </div>
         `;
     }
 }
