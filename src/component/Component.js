d6d0794050b070379e390ca87ae59dc0ae6101f7
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-22 15:45:00 -0700

    updated Component from configs file

--- src/component/Component.js
+++ src/component/Component.js
@@ -1,21 +1,30 @@
-import htmlToDOM from '../html-to-dom.js';
+import htmlToDOM from '../html-to-DOM.js';
 
 class Component {
     constructor(props) {
         this.props = props;
+        this.state = {};
     }
-
+    
     render() {
         return this.renderDOM();
     }
+
     renderDOM() {
         const html = this.renderTemplate();
         const dom = htmlToDOM(html);
+        // remember the dom for later
+        // for replacing or removing
         this.rootElement = dom;
         return dom;
     }
 
+    renderTemplate() {
+        throw new Error(`Component "${this.constructor.name}" needs to implement renderTemplate`);
+    }
+
     update(props) {
+        props = props || {};
         // update the props:
         Object.keys(props).forEach(key => {
             this.props[key] = props[key];
