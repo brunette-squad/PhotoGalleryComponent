f8cb43081a94207e078e5a04df43237d2a5c1361
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-23 11:46:03 -0700

    travis, once again

--- src/component/ImageItem.js
+++ src/component/ImageItem.js
@@ -4,7 +4,7 @@ class ImageItem extends Component {
     renderTemplate() {
         const image = this.props.image;
         return /*html*/ `
-            <li title="${image.description}">
+            <li>
                 <img src="${image.url}" alt="${image.description}">
                 <h3 class="title">${image.title}</h3>
             </li>
